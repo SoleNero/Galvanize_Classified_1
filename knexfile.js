c16e79ecc18401b5ced5f6f1953d4578a0fa8e6a
Justyna Foster <justynawfoster@gmail.com>
2017-01-30 10:54:54 -0700

    "add production in knexfile.js"

--- knexfile.js
+++ knexfile.js
@@ -4,16 +4,15 @@ module.exports = {
 
     development: {
         client: 'pg',
-        connection: {
-            database: process.env.DATABASE_URL || 'classifiedes_dev',
-        }
+        connection: 'postgres://localhost/classifieds_dev'
     },
-
     test: {
         client: 'pg',
-        connection: {
-            database: process.env.DATABASE_URL || 'classifiedes_test',
-        }
+        connection: 'postgres://localhost/classifieds_test'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
     }
 
 }
