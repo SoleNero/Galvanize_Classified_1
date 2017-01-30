c7f5c68a88aadb1accdc8607275c4ae565fa7025
Justyna Foster <justynawfoster@gmail.com>
2017-01-30 09:52:26 -0700

    router.get works

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,10 +1,22 @@
-
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+    knex('classifieds')
+        .select('id', 'title', 'description', 'price', 'item_image')
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+
+
+
 
 module.exports = router;
